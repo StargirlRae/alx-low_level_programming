fc0f48040b7a4923a87eb825878b80931f5af518
StargirlRae <raeiboyi52@gmail.com>
2023-09-14 09:04:43 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function checks for a digit (0 through 9)
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; c < 58; c++)
+	{
+		if (c != 50)
+		{
+			if (c != 52)
+			{
+				_putchar(c);
+			}
+		}
+	}
+	_putchar('\n');
+}
