4d4d21cea43246c31eba28511b62ff13f601aada
StargirlRae <raeiboyi52@gmail.com>
2023-09-15 04:31:20 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,9 +7,11 @@
 void print_number(int n)
 {
 	unsigned int num = n;
+
+	if (n < 0)
 	{
-		_putchar('_');
-		num = num;
+		_putchar('-');
+		num = -num;
 	}
 	if ((num / 10) > 0)
 		print_number(num / 10);
