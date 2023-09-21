114537abc8c82c2702b08790654dc3df0eb13630
StargirlRae <raeiboyi52@gmail.com>
2023-09-21 07:44:55 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
