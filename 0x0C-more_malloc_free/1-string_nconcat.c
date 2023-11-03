27ed71bbc49281db773669b7faf0344c7fe5ed4e
StargirlRae <raeiboyi52@gmail.com>
2023-11-02 17:14:25 -0700

    haeders

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,6 +13,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0, j = 0, k = 0, l = 0;
 	char *str;
+
 	if (s1 == NULL)
 	s1 = "";
 	if (s2 == NULL)
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
+/**
+ * is_digit  check if a string contains a non-digit char
+ * @s: string to be evaluated
+ * Return: 0 if a nondigit is found and 1 if digit is found
+ */
+int is_digit(char *s)
+{
+	int i = 0;
+	while ()
+i
+{
+	}
+	return ()
+}
